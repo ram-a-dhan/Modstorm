dcc8f67d2c4793f34484f95b42c44abccbf4d9ab
ram-a-dhan <ram.adhan@outlook.com>
2020-11-24 18:56:10 +0700

    removed unnecessary codes;

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -3,13 +3,10 @@ import {
   View,
   Text,
   StyleSheet,
-  useWindowDimensions,
 } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 
 export default function Splash({ route }) {
-  const { width } = useWindowDimensions();
-
   useEffect(() => {
     setTimeout(() => {
       route.params.setShowSplashScreen(false);
@@ -17,7 +14,7 @@ export default function Splash({ route }) {
   }, []);
 
   return (
-    <View style={[styles.center, styles.background, { width }]}>
+    <View style={[styles.center, { /* backgroundColor: '#181818' */ }]}>
       <View style={styles.flexRow}>
         <Animatable.View animation="fadeIn" delay={500}>
           <Animatable.Image
@@ -29,8 +26,8 @@ export default function Splash({ route }) {
           />
         </Animatable.View>
         <Animatable.View animation="fadeInRight" style={styles.flexRow}>
-          <Text style={styles.type1}>MOD</Text>
-          <Text style={styles.type2}>STORM</Text>
+          <Text style={[styles.type1, { /* color: 'white' */ }]}>MOD</Text>
+          <Text style={[styles.type2, { /* color: '#57a5cc' */ }]}>STORM</Text>
         </Animatable.View>
       </View>
     </View>
@@ -42,9 +39,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  background: {
-    // backgroundColor: '#181818',
   },
   flexRow: {
     flexDirection: 'row',
@@ -61,11 +55,9 @@ const styles = StyleSheet.create({
     fontSize: 40,
     margin: 0,
     marginLeft: 15,
-    // color: 'white',
   },
   type2: {
     fontFamily: 'BarlowCondensed-Bold',
     fontSize: 40,
-    // color: '#57a5cc',
   },
 });
