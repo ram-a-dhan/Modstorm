e1523d7c867667fe17d8d79175a41220f5aa0cf3
ram-a-dhan <ram.adhan@outlook.com>
2020-12-04 14:44:15 +0700

    finished basic layout for Games Screen with added responsiveness;

--- src/screens/Games.js
+++ src/screens/Games.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, {
+  useState,
+  useEffect,
+} from 'react';
 import {
   View,
   Text,
   StyleSheet,
   useWindowDimensions,
-  Image,
   ScrollView,
   ImageBackground,
   TextInput,
+  FlatList,
+  LogBox,
 } from 'react-native';
-import { FlatGrid } from 'react-native-super-grid';
+import {
+  TouchableOpacity,
+} from 'react-native-gesture-handler';
 import listOfGames from '../assets/listOfGames';
 
+LogBox.ignoreLogs([
+  'VirtualizedLists should never be nested inside plain ScrollViews with the same orientation'
+]);
+
 export default function Games() {
-  const { height } = useWindowDimensions();
+  const { width, height } = useWindowDimensions();
+  const [numColumns, setNumColumns] = useState(3);
+
+  useEffect(() => {
+    if (width > height) {
+      setNumColumns(5);
+    } else {
+      setNumColumns(3);
+    }
+  }, [width, height])
 
   return (
     <ScrollView stickyHeaderIndices={[1]}>
@@ -29,24 +48,34 @@ export default function Games() {
       <View style={{ backgroundColor: 'black', width: '100%', height: 50 }}>
         <TextInput placeholder="Search Game" style={{ textAlign: 'center', color: 'white', fontSize: 18 }} placeholderTextColor="darkgrey" />
       </View>
-      <FlatGrid
-        contentContainerStyle={{ width: '100%', marginTop: -25 }}
+      <FlatList
         ListHeaderComponent={() => (
           <View style={{ width: '100%', height: 50, marginBottom: 5, paddingHorizontal: 25, justifyContent: 'center' }}>
             <Text style={{ fontFamily: 'BarlowCondensed-Medium', fontSize: 28 }}>All Games</Text>
           </View>
         )}
         data={listOfGames}
-        keyExtractor={item => item.id}
+        keyExtractor={item => item.id.toString()}
         renderItem={({ item }) => (
           <ImageBackground
+            key={item.id.toString()}
             source={{ uri: `https://staticdelivery.nexusmods.com/Images/games/4_3/tile_${item.id}.jpg` }}
-            style={{ width: 150, aspectRatio: 0.8 }}
-          />
+            style={{ width: width / numColumns, aspectRatio: 0.8, justifyContent: 'flex-end' }}
+          >
+            <View style={{ padding: 10 }}>
+              <Text style={{ fontFamily: 'BarlowCondensed-Medium', fontSize: 15, color: 'white', marginBottom: 5 }}>
+                {item.name}
+              </Text>
+              <TouchableOpacity style={{ width: '100%', height: 20, backgroundColor: '#80808080', justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ fontFamily: 'BarlowCondensed-Regular', fontSize: 15, color: 'white' }}>
+                  Select
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </ImageBackground>
         )}
-        itemDimension={150}
-        spacing={25}
-        itemContainerStyle={{ justifyContent: 'center', alignItems: 'center' }}
+        numColumns={numColumns}
+        key={numColumns}
       />
     </ScrollView>
   );
