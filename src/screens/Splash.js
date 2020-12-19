dd22db202d0a887e959b7eb3ba71172366aae39e
ram-a-dhan <ram.adhan@outlook.com>
2020-12-19 20:04:28 +0700

    refactored Splash Screen;

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -7,27 +7,33 @@ import {
 import * as Animatable from 'react-native-animatable';
 
 export default function Splash({ route }) {
-  useEffect(() => {
-    setTimeout(() => {
-      route.params.setShowSplashScreen(false);
-    }, 3000);
-  }, []);
-
   return (
     <View style={[styles.center, { /* backgroundColor: '#181818' */ }]}>
       <View style={styles.flexRow}>
         <Animatable.View animation="fadeIn" delay={500}>
           <Animatable.Image
-            animation="rotate"
-            direction="reverse"
-            iterationCount="infinite"
             source={require('../assets/images/modstorm-logo.png')}
             style={styles.logo}
+            animation="rotate"
+            direction="reverse"
+            iterationCount={3}
+            onAnimationEnd={() => route.params.setShowSplashScreen(false)}
           />
         </Animatable.View>
-        <Animatable.View animation="fadeInRight" style={styles.flexRow}>
-          <Text style={[styles.type1, { /* color: 'white' */ }]}>MOD</Text>
-          <Text style={[styles.type2, { /* color: '#57a5cc' */ }]}>STORM</Text>
+        <Animatable.View
+          animation="fadeInRight"
+          style={styles.flexRow}
+        >
+          <Text
+            style={[styles.type1, { /* color: 'white' */ }]}
+          >
+            MOD
+          </Text>
+          <Text
+            style={[styles.type2, { /* color: '#57a5cc' */ }]}
+          >
+            STORM
+          </Text>
         </Animatable.View>
       </View>
     </View>
@@ -51,7 +57,7 @@ const styles = StyleSheet.create({
     height: 75,
   },
   type1: {
-    fontFamily: 'BarlowCondensed-Light',
+    fontFamily: 'BarlowCondensed-Regular',
     fontSize: 40,
     margin: 0,
     marginLeft: 15,
